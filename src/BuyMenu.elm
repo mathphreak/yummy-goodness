af3ed9653037fa1f81be8c8786020e078cc4bac3
Matt Horn <mathphreak@gmail.com>
2017-01-09 18:54:05 -0600

    Continue prototype buy menu

--- src/BuyMenu.elm
+++ src/BuyMenu.elm
@@ -1,9 +1,10 @@
 module BuyMenu exposing (view)
 
 import Html exposing (Html)
+import Html.Attributes
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Html.Events
+import Svg.Events exposing (..)
 
 
 -- VIEW
@@ -21,15 +22,58 @@ lineAtAngle angle =
         []
 
 
+wedge : Int -> Int -> String -> String -> msg -> Html msg
+wedge count idx top bottom msg =
+    let
+        startAngle =
+            (2 * pi * ((toFloat (idx * 2) + 1) / (toFloat count * 2))) + pi
+
+        midAngle =
+            (2 * pi * ((toFloat (idx * 2)) / (toFloat count * 2))) + pi
+
+        endAngle =
+            (2 * pi * ((toFloat (idx * 2) - 1) / (toFloat count * 2))) + pi
+
+        startX =
+            toString (50 + 50 * cos startAngle)
+
+        startY =
+            toString (50 + 50 * sin startAngle)
+
+        midX =
+            toString (50 + 30 * cos midAngle)
+
+        midY1 =
+            toString (47 + 30 * sin midAngle)
+
+        midY2 =
+            toString (53 + 30 * sin midAngle)
+
+        endX =
+            toString (50 + 50 * cos endAngle)
+
+        endY =
+            toString (50 + 50 * sin endAngle)
+    in
+        a [ class "wedge", onClick msg ]
+            [ Svg.path [ d ("M 50 50 L " ++ startX ++ " " ++ startY ++ " A 50 50 0 0 0 " ++ endX ++ " " ++ endY ++ " L 50 50 z"), fill "black" ] []
+            , text_ [ x midX, y midY1, fill "white", textAnchor "middle", fontSize "5px", dominantBaseline "middle" ] [ text top ]
+            , text_ [ x midX, y midY2, fill "white", textAnchor "middle", fontSize "5px", dominantBaseline "middle" ] [ text bottom ]
+            ]
+
+
 viewSix : msg -> Html msg
 viewSix msg =
-    svg [ viewBox "0 0 100 100", width "150px" ]
-        [ Svg.path [ d "M 50 50 L 6.7382812 25 A 50 50 0 0 0 0 50 A 50 50 0 0 0 6.7382812 75 L 50 50 z ", fill "#0A0A0A" ] []
-        , Svg.path [ d "M 50 50 L 6.7382812 75 A 50 50 0 0 0 50 100 L 50 50 z ", fill "#0A0A0A" ] []
-        , Svg.path [ d "M 50 50 L 50 100 A 50 50 0 0 0 93.261719 75 L 50 50 z ", fill "#0A0A0A" ] []
-        , Svg.path [ d "M 93.261719 25 L 50 50 L 93.261719 75 A 50 50 0 0 0 100 50 A 50 50 0 0 0 93.261719 25 z ", fill "#0A0A0A" ] []
-        , Svg.path [ d "M 50 0 A 50 50 0 0 0 6.7382812 25 L 50 50 L 50 0 z ", fill "#0A0A0A" ] []
-        , Svg.path [ d "M 50 0 L 50 50 L 93.261719 25 A 50 50 0 0 0 50 0 z ", fill "#0A0A0A" ] []
+    svg [ viewBox "0 0 100 100", width "300px" ]
+        [ wedge 6 1 "" "PISTOLS" msg
+        , wedge 6 2 "" "HEAVY" msg
+        , wedge 6 3 "" "SMGs" msg
+        , wedge 6 4 "" "RIFLES" msg
+        , wedge 6 5 "" "GEAR" msg
+        , wedge 6 6 "" "GRENADES" msg
+        , lineAtAngle (pi / 6)
+        , lineAtAngle (3 * pi / 6)
+        , lineAtAngle (5 * pi / 6)
         , circle [ cx "50", cy "50", r "10", fill "#FFFFFF" ] []
         , circle [ cx "50", cy "50", r "7.5", fill "#0000DD" ] []
         ]
@@ -37,7 +81,16 @@ viewSix msg =
 
 viewFour : msg -> Html msg
 viewFour msg =
-    svg [ viewBox "0 0 100 100", width "100px" ] []
+    svg [ viewBox "0 0 100 100", width "300px" ]
+        [ wedge 4 1 "" "PISTOLS" msg
+        , wedge 4 2 "" "HEAVY" msg
+        , wedge 4 3 "" "SMGs" msg
+        , wedge 4 4 "" "RIFLES" msg
+        , lineAtAngle (pi / 4)
+        , lineAtAngle (3 * pi / 4)
+        , circle [ cx "50", cy "50", r "10", fill "#FFFFFF" ] []
+        , circle [ cx "50", cy "50", r "7.5", fill "#0000DD" ] []
+        ]
 
 
 view : msg -> Int -> Html msg
